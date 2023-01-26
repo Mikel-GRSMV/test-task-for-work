2c13bf08fcaeb3650888ccd07f67b39cf2927ce9
Mikel-GRSMV <mikhail.gerasimov.2000@mail.ru>
2023-01-26 13:24:29 +0300

    First commit

---
+++ src/main/java/ru/folder/pastabox/PastaBoxApplication.java
@@ -0,0 +1,13 @@
+package ru.folder.pastabox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastaBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PastaBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/folder/pastabox/PastaBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.folder.pastabox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastaBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
